8c803c04e1a0437aa251d0eb950005becd7f9d42
murayama <murayama333@gmail.com>
2015-09-18 12:15:52 +0900

    update device-width.

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>JSaiten</title>
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/style.css">
